500a0b685e3907201e7621ca266b276afd41e3ed
67051051 <67051051@kmitl.ac.th>
2026-09-18 18:57:00 +0700

    Add calculator functionality with HTML, CSS, and JS files

---
+++ cell03/ex04/ex02bis/calc.js
@@ -0,0 +1,45 @@
+$(document).ready(function () {
+    $("#calculate").on("click", function () {
+        const a = Number($("#left").val());
+        const b = Number($("#right").val());
+        const op = $("#operator").val();
+
+        if (
+            $("#left").val() === "" ||
+            $("#right").val() === "" ||
+            !Number.isInteger(a) ||
+            !Number.isInteger(b) ||
+            a < 0 ||
+            b < 0
+        ) {
+            alert("Error :(");
+            return;
+        }
+
+        if ((op === "/" || op === "%") && b === 0) {
+            alert("It's over 9000!");
+            return;
+        }
+
+        let result;
+
+        if (op === "+") {
+            result = a + b;
+        } else if (op === "-") {
+            result = a - b;
+        } else if (op === "*") {
+            result = a * b;
+        } else if (op === "/") {
+            result = a / b;
+        } else if (op === "%") {
+            result = a % b;
+        }
+
+        alert(result);
+        console.log(result);
+    });
+
+    setInterval(function () {
+        alert("Please, use me...");
+    }, 30000);
+});
